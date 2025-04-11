43c27eef238df217c7a668ee65dfa19fecebe622
Nurbek Dohdyrbek <87654975+nurbek18@users.noreply.github.com>
2025-04-11 06:52:21 +0000

    为了使sitemap的正确性，添加了robots 文件，并在main 中也添加了robots的独有的code

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ from flask import send_from_directory
 def sitemap():
     return send_from_directory('.', 'sitemap.xml')
 
+# Robots.txt 路由 ✅ 新增这部分
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory('.', 'robots.txt')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
